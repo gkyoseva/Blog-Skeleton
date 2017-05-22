b8d754bd48a03785e31ec4ac39fcbb8c7fe5b8e1
gkyoseva <ginka.kyoseva@gmail.com>
2017-05-22 21:30:42 +0300

    Changed port

--- Block.UI.Tests/BrowserHost.cs
+++ Block.UI.Tests/BrowserHost.cs
@@ -13,10 +13,10 @@ namespace Block.UI.Tests
     {
         public static readonly SelenoHost Instance = new SelenoHost();
         public static readonly string RootUrl = @"http://localhost:60634/Article/List";
-
+        
         static BrowserHost()
         {
-            Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
+            Instance.Run("Blog", 60639, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
         }
     }
 }
